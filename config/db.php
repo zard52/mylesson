9cd7ccd130b6a3777c1863f0bc4bb03e1e697f65
unknown <addtim52@gmail.com>
2022-07-26 11:17:29 +0800

    initial code

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=my_lesson',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ migrations/m220726_023211_create_lesson_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%lesson}}`.
+ */
+class m220726_023211_create_lesson_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%lesson}}', [
+            'id' => $this->primaryKey(),
+            'tutor_id' => $this->integer()->notNull(),
+            'student_id' => $this->integer()->notNull(),
+            'start' => $this->datetime(),
+            'end' => $this->datetime(),
+            'status' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%lesson}}');
+    }
+}
